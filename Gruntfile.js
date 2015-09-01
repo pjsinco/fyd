7f965589e7f00901f0b6a07a3a37bf1c084a9827
pjsinco <psinco@fas.harvard.edu>
2015-09-01 10:33:51 -0500

    Fix gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -101,9 +101,9 @@ module.exports = function(grunt) {
     } // watch
   }); // initConfig
   
-  grunt.registerTask('compile sass', ['compass:dev', 'notify:sass']);
-  grunt.registerTask('compile jade', ['jade', 'notify:jade']);
-  grunt.registerTask('compile js', ['browserify:dev', 'notify:browserify']);
+  grunt.registerTask('compile-sass', ['compass:dev', 'notify:sass']);
+  grunt.registerTask('compile-jade', ['jade', 'notify:jade']);
+  grunt.registerTask('compile-js', ['browserify:dev', 'notify:browserify']);
   grunt.registerTask('default', ['watch']);
 
 }; // exports
