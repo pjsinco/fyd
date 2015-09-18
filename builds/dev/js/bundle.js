7833c57574daa4bf63ef2273b7e51e4fd75dc3c9
pjsinco <psinco@fas.harvard.edu>
2015-09-18 18:26:03 -0500

    Complete an iteration on styling the physician list

--- builds/dev/js/bundle.js
+++ builds/dev/js/bundle.js
@@ -2795,11 +2795,11 @@ var PhysicianListItemView = Backbone.View.extend({
     },
 
     template: _.template(
+            '<h6 class="fyd-results__kicker"><%= specialty %></h6>' +
             '<h3 class="fyd-results__name">' +
                 '<a href="results.html#physicians/<%= id %>"><%= full_name %></a>' +
-            '<h3>' +
-            '<h4><%= specialty %></h4>' +
-            '<div><p data-lat=<%= lat %> data-lon=<%= lon %>>' +
+            '</h3>' +
+            '<div class="fyd-results__body"><p data-lat=<%= lat %> data-lon=<%= lon %>>' +
                 '<%= addr_1 %><br/ >' +
                 '<%= city %>, <%= state %> <%= zip %>' +
             '</p></div>'
